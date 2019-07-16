20ba6da1c867560ce93f532acd70ab9284614a4b
Yoga 710 <Yoga 710@DESKTOP-LNDFM54.mshome.net>
2019-07-16 18:55:44 +0530

    HelloWorld

---
+++ myproject/src/day1/HelloWorld.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class HelloWorld {
+	static void getMessage(){
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		getMessage();
+		// TODO Auto-generated method stub
+
+	}
+
+}
